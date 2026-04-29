0037bc0d7eddfebffc3d25e29b1ad4b3e49119d0
AmmarKhan21 <khanammar668@gmail.com>
2026-04-29 23:42:23 +0500

    Navigation and required packages according to the requirments added

--- android/app/src/main/java/com/imagegalleryapp/MainActivity.kt
+++ android/app/src/main/java/com/imagegalleryapp/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.imagegalleryapp
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.swmansion.rnscreens.fragment.restoration.RNScreensFragmentFactory
 
 class MainActivity : ReactActivity() {
 
@@ -12,6 +14,15 @@ class MainActivity : ReactActivity() {
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "imagegalleryapp"
+
+  /**
+   * Required by react-native-screens to prevent crashes related to View state
+   * not being persisted across Activity restarts on Android.
+   */
+  override fun onCreate(savedInstanceState: Bundle?) {
+    supportFragmentManager.fragmentFactory = RNScreensFragmentFactory()
+    super.onCreate(savedInstanceState)
+  }
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
